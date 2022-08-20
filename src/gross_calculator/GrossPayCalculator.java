8ee5c3a2b9e39c13c907f5fe7120d50c13ccbb8e
MariyaCatLady <mariyazelikman@gmail.com>
2022-08-19 22:51:11 -0700

    Added basic code - calculator program and user input program.

---
+++ src/gross_calculator/GrossPayCalculator.java
@@ -0,0 +1,30 @@
+package gross_calculator;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+
+    public static void main(String[] args){
+        //1. Get the number of hours worked
+        int hours;
+        System.out.println("How many hours did you work? ");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+
+        //2. Get the hourly pay rate
+        double payRate;
+        System.out.println("What is your pay rate? ");
+        payRate = scanner.nextDouble();
+
+        scanner.close();
+
+        //3. Multiply hours and pay rate
+        final double grossPay = hours * payRate;
+
+        //4. Display result
+        System.out.println("Gross Pay: " + grossPay);
+
+
+    }
+}
