261fee814616599d3feccbe79abc92a0e42ff568
MariyaCatLady <mariyazelikman@gmail.com>
2022-09-07 19:08:38 -0700

    Water Bill program - calculates the water bill taking into account additional charges for extra gallons used.

---
+++ src/JaveCodeChallenges/WaterBill.java
@@ -0,0 +1,43 @@
+package JaveCodeChallenges;
+
+import java.util.Scanner;
+
+public class WaterBill {
+    public static void main(String[] args) {
+        System.out.println("Your water bill is $" + calculateWaterBill(inputAndValidation()));
+    }
+
+    public static double calculateWaterBill(double gallonsUsage) {
+        if (gallonsUsage == 0) {
+            return 0;
+        }
+
+        final double MIN_GALLONS = 1496;
+        final double MIN_CHARGES = 18.84;
+
+        if (gallonsUsage > MIN_GALLONS) {
+            return calculateAdditionalCharges(gallonsUsage, MIN_GALLONS, MIN_CHARGES);
+        }
+
+        return MIN_CHARGES;
+    }
+
+    public static double inputAndValidation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many gallons of water did you use this month? ");
+        double gallonsUsed = scanner.nextDouble();
+        while (gallonsUsed < 0) {
+            System.out.println(gallonsUsed + " is an invalid input. Please enter a valid number of gallons.\n");
+            System.out.println("How many gallons of water did you use this month? ");
+            gallonsUsed = scanner.nextDouble();
+        }
+        return gallonsUsed;
+    }
+
+    public static double calculateAdditionalCharges(double gallonsUsed, double minGallons, double minCharge) {
+        double CCFs = Math.ceil((gallonsUsed - minGallons) / 748);
+        final double ADDITIONAL_CHARGES = 3.90;
+        return minCharge + (CCFs * ADDITIONAL_CHARGES);
+    }
+
+}
