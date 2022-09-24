3133a337c915ef8bbb596194202ab5cb38640f61
MariyaCatLady <mariyazelikman@gmail.com>
2022-09-24 11:35:22 -0700

    Added basic code- method isEven(int num) returns true if num is even and false if it's odd.

--- src/JaveCodeChallenges/EvenOrOdd.java
+++ src/JaveCodeChallenges/EvenOrOdd.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
