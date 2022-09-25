aa3463436531cebbedf96b5a4a0315f1c287ec59
MariyaCatLady <mariyazelikman@gmail.com>
2022-09-25 16:11:54 -0700

    The StoreItem class models an item with three attributes, name, price, and discount. It contains three methods. One that calculates the discounted price of an item. The other one returns the least expensive discounted item. The last one formats the output.

--- src/JavaCodeChallenges/StoreItemMain.java
+++ src/JavaCodeChallenges/StoreItemMain.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class StoreItemMain {
     public static void main(String[] args) {
+
         List<StoreItem> items = List.of(new StoreItem("soap", 5.00, 0.5),
                 new StoreItem("candles", 9.00, 0.9),
                 new StoreItem("juice", 9.00, 0.4),
