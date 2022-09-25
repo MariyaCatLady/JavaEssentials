ffdbbc5426d3d28176584151a9081303b80c9f02
MariyaCatLady <mariyazelikman@gmail.com>
2022-09-25 16:10:22 -0700

    The StoreItem class models an item with three attributes, name, price, and discount. It contains three methods. One that calculates the discounted price of an item. The other one returns the least expensive discounted item. The last one formats the output.

--- src/JavaCodeChallenges/StoreItem.java
+++ src/JavaCodeChallenges/StoreItem.java
@@ -1,31 +1,46 @@
 package JavaCodeChallenges;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Optional;
+import java.text.DecimalFormat;
+import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class StoreItem {
-    String name;
-    double retailPrice;
-    double discount;
+    //data fields
+    final private String name;
+    final private double price;
+    final private double discount;
 
-    public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
-        return items.stream().min(Comparator.comparing(StoreItem::calculateCurrentPrice));
+    public StoreItem(String itemName, double itemPrice, double itemDiscount) {
+        name = itemName;
+        price = itemPrice;
+        discount = itemDiscount;
     }
 
-    private double calculateCurrentPrice() {
-        return retailPrice - (retailPrice * discount);
+    private double calculateDiscountPrice() {
+        return price - (price * discount);
+    }
+
+    public static StoreItem findLeastExpensiveItem(List<StoreItem> items) {
+        if (items.isEmpty()) {
+            return null;
+        }
+
+        StoreItem minItem = items.get(0);
+        for (int i = 1; i < items.size(); i++) {
+            StoreItem item = items.get(i);
+            if (item.calculateDiscountPrice() < minItem.calculateDiscountPrice()) {
+                minItem = item;
+            }
+        }
+
+        return minItem;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + ", " + "Retail price: " + retailPrice + ", " + "Discount " + discount;
+        DecimalFormat df = new DecimalFormat("##.00");
+        DecimalFormat df2 = new DecimalFormat("##.0");
+
+        return "Name: " + name + ", discount: " + df2.format((discount * 100)) + "% , discounted cost: $" + df.format(calculateDiscountPrice());
     }
 }
+
---
+++ src/JavaCodeChallenges/StoreItemMain.java
@@ -0,0 +1,18 @@
+package JavaCodeChallenges;
+
+import java.util.List;
+
+public class StoreItemMain {
+    public static void main(String[] args) {
+        List<StoreItem> items = List.of(new StoreItem("soap", 5.00, 0.5),
+                new StoreItem("candles", 9.00, 0.9),
+                new StoreItem("juice", 9.00, 0.4),
+                new StoreItem("cereal", 9.00, 0.6));
+
+        System.out.println(StoreItem.findLeastExpensiveItem(items));
+
+
+    }
+
+
+}
